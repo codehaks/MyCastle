de24e16558a7faa2226726e7828dbef55ec03ff7
Hakim Ghods <ghods.hakim@hotmail.com>
2019-03-09 21:36:41 +0330

    Add project files.

---
+++ MyCastle/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MyCastle.Data;
+
+namespace MyCastle
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+               options.UseSqlite("Data Source=app.sqlite"));
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddEntityFrameworkStores<AppDbContext>();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                //options.User.RequireUniqueEmail = false;
+                //options.Password.RequiredUniqueChars = 0;
+                //options.Password.RequiredLength = 6;
+                //options.SignIn.RequireConfirmedEmail = false;
+                //options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromDays(3);     
+            });
+
+            services.Configure<CookieAuthenticationOptions>(options =>
+            {
+                options.ExpireTimeSpan = TimeSpan.FromDays(1);
+                options.Cookie.Expiration = TimeSpan.FromDays(2);
+            });
+
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
